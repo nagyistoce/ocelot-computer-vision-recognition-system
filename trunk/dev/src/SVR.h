1f7bbf7fde9bfeb9fb00f8c10302d1369cd49f4d
wwagner006@pointloma.edu <wwagner006@pointloma.edu@7b3da652-4197-5a74-260e-2931f2a0810e>
2012-12-03 22:31:32 +0000

    C++ code for SRV1 initial add

---
+++ trunk/dev/src/SVR.h
@@ -0,0 +1,22 @@
+#include "posixSoc.h"
+
+
+// This file selects which type of Surveyor object to create
+// un-comment out the one for your OS
+// and comment out the other
+//
+// and change the following to your robots address
+
+// and change the following to your robots address
+//
+//if your Surveyor is at address 192.168.1.191 port 10001
+// you can use the following line
+//#define ADDRESS		"192.168.1.191:10001"
+//
+// or if you want to get the address string form the environment variable 
+// SVR_ADDRESS use the following line
+//#define ADDRESS		NULL
+
+#define ADDRESS		NULL
+
+
