e2dcf97534536d9b499f67429e80b89f036befc5
wwagner006@pointloma.edu <wwagner006@pointloma.edu@7b3da652-4197-5a74-260e-2931f2a0810e>
2012-12-07 05:50:41 +0000

    placed temp code in src dir

--- trunk/dev/src/x.bat
+++
@@ -1,1 +0,0 @@
-set PATH=c:\WINDOWS\system32;C:\WINDOWS;C:\MinGW\bin;C:\Program Files\CodeBlocks\bin;
--- trunk/dev/winsetup.bat
+++
@@ -1,7 +0,0 @@
-copy src\Windows.Sockets src\MyOS.Sockets
-copy src\WinHeader src\SVR.h
-echo it is necessary to set the address of your robot
-echo either in src\SVR.h or in environment variable SVR_ADDRESS
-
-
-
